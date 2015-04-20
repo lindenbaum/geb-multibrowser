bf38acec1f400fc5538f2e339feb7866711771f6
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2015-04-20 15:59:04 +0200

    fixed reporting spec

--- MultiBrowserReportingGebSpec.groovy
+++ MultiBrowserReportingGebSpec.groovy
@@ -6,27 +6,30 @@ import org.junit.rules.TestName
 import geb.report.ReporterSupport
 
 abstract class MultiBrowserReportingGebSpec extends MultiBrowserGebSpec {
-	// Ridiculous name to avoid name clashes
-	@Rule TestName _gebReportingSpecTestName
-	def _gebReportingPerTestCounter = 1
-	@Shared _gebReportingSpecTestCounter = 1
-	@Shared haveCleanedReportDir
+  // Ridiculous name to avoid name clashes
+  @Rule TestName _gebReportingSpecTestName
+  def _gebReportingPerTestCounter = 1
+  @Shared _gebReportingSpecTestCounter = 1
+  @Shared haveCleanedReportDir
 
-	def cleanup() {
-		report "end"
-	}
+  def cleanup() {
+    report "end"
+  }
 
-	void report(String label = "") {
-		if(!haveCleanedReportDir) {
-			haveCleanedReportDir = true
-			browser.cleanReportGroupDir()
-		}
-		def testClass = getClass()
-		def baseReportName = ReporterSupport.toTestReportLabel(_gebReportingSpecTestCounter++, _gebReportingPerTestCounter++, _gebReportingSpecTestName.methodName, label)
-		_browsers.each { key, browser ->
-			browser.reportGroup testClass
-			browser.report(baseReportName + "[$key]")
-		}
-	}
+  void report(String label = "") {
+    if(!haveCleanedReportDir) {
+      haveCleanedReportDir = true
+      browser.cleanReportGroupDir()
+    }
+    def testClass = getClass()
+    def baseReportName = ReporterSupport.toTestReportLabel(_gebReportingSpecTestCounter++, _gebReportingPerTestCounter++, _gebReportingSpecTestName.methodName, label)
+    def i=1
+    _browsers.each { browser ->
+      browser.reportGroup testClass
+      browser.report(baseReportName + "[$i]")
+      i += 1
+    }
+    _defaultBrowser?.reportGroup testClass
+    _defaultBrowser?.report(baseReportName)
+  }
 }
-
